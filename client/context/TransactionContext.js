52a917cca3c65e1df43b2f892028ea5db7d8a872
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 15:34:51 +0300

    Rokas

---
+++ client/context/TransactionContext.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+
+export const TransactionContext = React.createContext()
+
+let eth
+
+if(typeof window !== 'undefined') {
+    eth = window.ethereum
+}
+
+const TransactionProvider = ({children}) => {
+    const [currentAccount, setCurrentAccount] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+}
+
+const connectWallet = async (metamask = eth) => {
+    try {
+        if(!metamask) return alert('Please install MetaMask');
+        const accounts = await metamask.request({method: 'eth_requestAccounts'});
+        setCurrentAccount(accounts[0]);
+    }
+}
+
