0f21f36ff54914cd8ecd9392cabee08467d3d15f
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 15:11:16 +0300

    Rokas

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header';
 
 const style = {
     wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
         <title>CryptoSwap App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+        <Header />
     </div>
   )
 }
