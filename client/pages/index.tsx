7421af9ee097e45d8d5ecdedb2ae8788a254ec7b
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 15:26:12 +0300

    Rokas

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header';
+import Main from '../components/Main';
+
 
 const style = {
     wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -15,6 +17,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Header />
+        <Main />
     </div>
   )
 }
