9c598e7fe8546d12efcdecaf007148cd614a0f56
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 16:09:05 +0300

    Rokas

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/k4W_geBi6cR-U2YMehY-h0W80gyVgcTm',
+      accounts: [
+          'add6b8684b20e6d7ea41e12e5c4d41b38c4d6413a9dd4cbc6c3792e3ba631abd'
+      ],
+    },
+  },
 };
