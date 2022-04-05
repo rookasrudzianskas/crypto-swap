6caad38a817384ac860af731bd635c4e50fdfd56
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 15:08:11 +0300

    Rokas

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>CryptoSwap App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
