4631384f8a180970c6b0cf4a39519bfca48f1964
Rokas <rookas.rudzenskas@gmail.com>
2022-04-06 09:06:31 +0300

    Rokas

--- client/components/Main.js
+++ client/components/Main.js
@@ -7,6 +7,7 @@ import { useContext } from 'react'
 import {TransactionContext} from "../context/TransactionContext";
 import {useRouter} from "next/router";
 import Modal from 'react-modal';
+import TransactionLoader from '../components/TransactionLoader';
 
 Modal.setAppElement('#__next');
 
@@ -23,6 +24,7 @@ const style = {
     currencySelectorArrow: `text-lg`,
     confirmButton: `bg-[#2172E5] my-2 rounded-2xl py-6 px-8 text-xl font-semibold flex items-center justify-center cursor-pointer border border-[#2172E5] hover:border-[#234169]`,
 }
+
 
 const customStyles = {
     content: {
