d1ba63a89d0fa7a4513378e94e4dbe3e43b75490
Rokas <rookas.rudzenskas@gmail.com>
2022-04-06 08:09:43 +0300

    ROkas

--- client/context/TransactionContext.js
+++ client/context/TransactionContext.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useRouter} from "next/router";
 import {contractABI, contractAddress} from '../lib/constants';
-import { ehters } from 'ethers';
+import { ethers } from 'ethers';
 // import {client} from '../lib/sanityClient';
 
 export const TransactionContext = React.createContext()
